86bc1ad68ed7bc952b7f0b325529972e6525cb91
arvidn <arvid@libtorrent.org>
2021-05-08 09:26:02 +0200

    formatting

--- tests/core/types/test_coin.py
+++ tests/core/types/test_coin.py
@@ -4,6 +4,7 @@ from chia.util.ints import uint64
 from chia.util.hash import std_hash
 import io
 
+
 def coin_serialize(amount: uint64, clvm_serialize: bytes, full_serialize: bytes):
 
     c = Coin(bytes32(b"a" * 32), bytes32(b"b" * 32), amount)
@@ -24,10 +25,9 @@ def coin_serialize(amount: uint64, clvm_serialize: bytes, full_serialize: bytes)
 
 
 class TestCoin:
-
     def test_coin_serialization(self):
 
-        coin_serialize(uint64(0xffff), bytes([0, 0xFF, 0xFF]), bytes([0, 0, 0, 0, 0, 0, 0xFF, 0xFF]))
+        coin_serialize(uint64(0xFFFF), bytes([0, 0xFF, 0xFF]), bytes([0, 0, 0, 0, 0, 0, 0xFF, 0xFF]))
         coin_serialize(uint64(1337000000), bytes([0x4F, 0xB1, 0x00, 0x40]), bytes([0, 0, 0, 0, 0x4F, 0xB1, 0x00, 0x40]))
 
         # if the amount is 0, the amount is omitted in the "short" format,
@@ -36,7 +36,8 @@ class TestCoin:
 
         # when amount is > INT64_MAX, the "short" serialization format is 1 byte
         # longer, since it needs a leading zero to make it positive
-        coin_serialize(uint64(0xffffffffffffffff),
-            bytes([0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]),
-            bytes([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]))
-
+        coin_serialize(
+            uint64(0xFFFFFFFFFFFFFFFF),
+            bytes([0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]),
+            bytes([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]),
+        )
